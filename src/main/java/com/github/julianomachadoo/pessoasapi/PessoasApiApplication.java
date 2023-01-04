4757d37493d4fbec81057c0e52b7f8651b463b9e
julianomachadoo <julmachadoramos@gmail.com>
2023-01-04 10:57:46 -0300

    Projeto criado

---
+++ src/main/java/com/github/julianomachadoo/pessoasapi/PessoasApiApplication.java
@@ -0,0 +1,16 @@
+package com.github.julianomachadoo.pessoasapi;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+public class PessoasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PessoasApiApplication.class, args);
+	}
+
+}
+
